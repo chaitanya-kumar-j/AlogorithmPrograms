511697b1e54a1ac45ae9dafd8fdda00f9f32244a
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-05 21:23:04 +0530

    [Chaitanya] Add : Added word search in file

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -6,7 +6,33 @@ namespace AlgorithmPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // run or exit
+            bool IsRun = true;
+            while (IsRun)
+            {
+                // Select Program number and enter
+                Console.WriteLine("Select and enter program number: " +
+                    "(1. Search Word)");
+                int programOption = Convert.ToInt32(Console.ReadLine());
+                switch (programOption)
+                {
+                    case 1:
+                        string filePath = @"G:\BridgeLabz\AlogorithmPrograms\AlgorithmPrograms\ListOfWords.txt";
+                        bool IsInList = new SearchWord().IsInList(filePath);
+                        if (IsInList)
+                        {
+                            Console.WriteLine("The searched word is in file.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The searched word is not in file.");
+                        }
+                        break;
+                    default:
+                        IsRun = !IsRun;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ AlgorithmPrograms/SearchWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class SearchWord
+    {
+        public bool IsInList(string filePath)
+        {
+            string data = File.ReadAllText(filePath);
+            string[] ListOfWords = data.Split(",");
+            Console.WriteLine("Enter the word to search: ");
+            string wordToSearch = Console.ReadLine().ToLower();
+            foreach(string word in ListOfWords)
+            {
+                if(wordToSearch == word.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
